b4d1c923ec2e453cff7aa8aa4f106b7c4f3b3e7b
angelajasso <angelajasso@hotmail.com>
2025-01-26 22:33:42 -0600

    adding mode dark

--- script.js
+++ script.js
@@ -18,3 +18,22 @@ function updateCarousel() {
   const offset = -currentIndex * 100;
   carousel.style.transform = `translateX(${offset}%)`;
 }
+
+const preferedColorScheme = window.matchMedia("(prefers-color-scheme: dark)")
+  .matches
+  ? "dark"
+  : "light";
+const slider = document.getElementById("slider");
+
+const setTheme = (theme) => {
+  document.documentElement.setAttribute("data-theme", theme);
+  localStorage.setItem("theme", theme);
+};
+
+slider.addEventListener("click", () => {
+  let switchToTheme =
+    localStorage.getItem("theme") === "dark" ? "light" : "dark";
+  setTheme(switchToTheme);
+});
+
+setTheme(localStorage.getItem("theme") || preferedColorScheme);
